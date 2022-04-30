9b795f4a9a2d729730397f51beeb87e4f1ac9069
judah <87juda@gmail.com>
2022-04-30 15:42:24 -0300

    Add bookstore initial structure

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
+// App.js
 /* eslint-disable react/jsx-one-expression-per-line */
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import WebHeader from './components/WebHeader';
+import WebHome from './components/WebHome';
+import WebCategories from './components/WebCategories';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Edit <code>src/App.js</code> and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <WebHeader />
+      <Routes>
+        <Route exact path="/" element={<WebHome />} />
+        <Route path="/WebCategories" element={<WebCategories />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/WebHome.js
@@ -0,0 +1,21 @@
+// WebHome.js
+
+import './styles/WebHome.css';
+
+function WebHome() {
+  return (
+    <div className="WebPage">
+      <h2 className="HeaderTitle">
+        Welcome to the Bookstore!
+      </h2>
+      <div className="text">
+        <p>
+          A room without books is like a body without a soul.
+          ― Marcus Tullius Cicero
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default WebHome;
